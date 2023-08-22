69c52219ba62273a400645c5edb36ab30bf86a2f
luylucas10 <luylucas10@gmail.com>
2023-08-21 23:27:23 -0300

    Inicial
    Criação do projeto MVC

---
+++ src/CursoInicianteMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CursoInicianteMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
